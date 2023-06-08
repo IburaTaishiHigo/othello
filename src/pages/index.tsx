e133a07b0ef4837e2059f6872d3e3d4fda46934e
IburaTaishiHigo <s1F102300526@iniad.org>
2023-06-09 05:52:05 +0900

    'オセロがエラーありで一応完成'

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,14 +13,52 @@ const Home = () => {
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
   ]);
+  const dirs: { y: number; x: number }[] = [
+    { y: -1, x: -1 },
+    { y: -1, x: 0 },
+    { y: -1, x: 1 },
+    { y: 0, x: 1 },
+    { y: 1, x: 1 },
+    { y: 1, x: 0 },
+    { y: 1, x: -1 },
+    { y: 0, x: -1 },
+  ];
+
   const onClick = (x: number, y: number) => {
     console.log(x, y);
     const newBoard: number[][] = JSON.parse(JSON.stringify(board));
-    if (board[y + 1] !== undefined && board[y + 1][x] !== 0 && board[y + 1][x] !== turnColor) {
-      newBoard[y][x] = turnColor;
-      setTurnColor(3 - turnColor);
+    for (const dir of dirs) {
+      console.log(dir);
+      for (let i = 1; i < 8; i++) {
+        if (board[y + dir.y * i] === undefined) {
+          console.log('a');
+          break;
+        }
+        if (board[x + dir.x * i] === undefined) {
+          console.log('b');
+          break;
+        } else if (board[y + dir.y * i][x + dir.x * i] === turnColor) {
+          console.log('c');
+          break;
+        } else if (board[y + dir.y * i][x + dir.x * i] === 0) {
+          console.log('d');
+          break;
+        } else if (
+          board[y + dir.y * i][x + dir.x * i] !== turnColor &&
+          board[y + dir.y * (i + 1)][x + dir.x * (i + 1)] === turnColor
+        ) {
+          console.log(i);
+          newBoard[y][x] = turnColor;
+          for (let m = 1; m < i + 1; m++) {
+            newBoard[y + dir.y * m][x + dir.x * m] = turnColor;
+            console.log(m);
+          }
+          setTurnColor(3 - turnColor);
+          break;
+        }
+      }
+      setBoard(newBoard);
     }
-    setBoard(newBoard);
   };
   return (
     <div className={styles.container}>
