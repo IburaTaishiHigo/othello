22fc27d89f3b65a20c832a59341efd4a51c98d0b
IburaTaishiHigo <IburaTaishiHigo@users.noreply.github.com>
2023-06-22 13:19:35 +0000

    deploy: 9942ae175edc676a27b244c345b346d4e46d5ee0

---
+++ _next/static/PCUu8wz1w8yJl0JKhaIKI/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/11a164be740b84c1.css","static/chunks/pages/index-4290e7aaaf0d97f7.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/aKdfXdfH0s5sMpYmz7v_D/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/11a164be740b84c1.css","static/chunks/pages/index-107a7eeac4ef0c84.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
